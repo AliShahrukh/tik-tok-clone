290d0b57ff362dc2cec5e25744fa86fce2dd9bab
Shahrukh <37803285+AliShahrukh@users.noreply.github.com>
2020-08-21 16:22:40 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbEBVt0IrrWHOdvp_j7vVFQRQjA__Tkso",
+    authDomain: "tik-tok-clone-e8781.firebaseapp.com",
+    databaseURL: "https://tik-tok-clone-e8781.firebaseio.com",
+    projectId: "tik-tok-clone-e8781",
+    storageBucket: "tik-tok-clone-e8781.appspot.com",
+    messagingSenderId: "731881832581",
+    appId: "1:731881832581:web:524d94064261a733cda9ec",
+    measurementId: "G-LEBZG322XD"
+  };
