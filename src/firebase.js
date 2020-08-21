060ca1fc6a378342c8d28158159f8851e5446a22
Shahrukh <37803285+AliShahrukh@users.noreply.github.com>
2020-08-21 17:38:32 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbEBVt0IrrWHOdvp_j7vVFQRQjA__Tkso",
+  authDomain: "tik-tok-clone-e8781.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-e8781.firebaseio.com",
+  projectId: "tik-tok-clone-e8781",
+  storageBucket: "tik-tok-clone-e8781.appspot.com",
+  messagingSenderId: "731881832581",
+  appId: "1:731881832581:web:524d94064261a733cda9ec",
+  measurementId: "G-LEBZG322XD"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
